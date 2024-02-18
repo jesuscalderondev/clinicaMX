6317bf13c281c80e82d88c0d5a391f390d985653
jesuscalderondev <jesuscalderonunimag@gmail.com>
2024-02-17 20:34:34 -0500

    Siuuuuu

--- apis.py
+++ apis.py
@@ -46,7 +46,7 @@ def consularHorario(fecha):
 @apis.route('/sala')
 def consultarTurnoSala():
     try:
-        turnosObj = session.query(Turno).filter(Turno.fecha == date.today(), Turno.asiste == 'Pendiente').order_by(Turno.hora).limit(5)
+        turnosObj = session.query(Turno).filter(Turno.fecha == date.today(), Turno.asiste == 'Pendiente').order_by(Turno.hora).limit(5).all()
         print(turnosObj)
         turnos = []
 
